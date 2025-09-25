6c5f48814451baf0363da5c49fe898498d9ce34c
Lm <egg156471@gmail.com>
2025-09-25 09:24:14 +0700

    genai

---
+++ python.py
@@ -0,0 +1,9 @@
+from google import genai
+import os
+os.getenv 
+client = genai.Client()
+response = client.models.generate_content(
+    model="gemini-2.5-flash",
+    contents="Explain how AI works in a few words"
+)
+print(response.text)   
